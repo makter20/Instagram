e49def779c8711cf18540103a968418deb33e2f7
makter20 <74988056+makter20@users.noreply.github.com>
2021-03-13 23:43:37 -0800

    part1

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "wGHLSGKZVMoR0NUpQcYt57G9I8QTiKHuP3CR515S" // <- UPDATE
+                $0.clientKey = "wUue1pGmpqHGiLg4tOou5KT71QhLG95wk1UUnDlw" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
@@ -30,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
---
+++ Instagram/PostTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PostTableViewCell.swift
+//  Instagram
+//
+//  Created by Mahmuda Akter on 3/13/21.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
